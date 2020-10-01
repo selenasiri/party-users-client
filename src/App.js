67394477654afd6ca9c7d108f838aae3336537b0
Selena Siri <selenasiri@gmail.com>
2020-10-01 16:40:49 -0400

    submit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import './App.css'
 function App() {
   const [name, setName] = useState('')
 
+  const onSubmitForm = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch(`http://localhost:5000/users/?name=${name}`)
+
+      const parseResponse = await response.json()
+
+      console.log(parseResponse)
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
   return (
     <Fragment>
       <div className='container text-center'>
         <h1 className='my-5'>Party List</h1>
-        <form className='d-flex'>
+        <form className='d-flex' onSubmit={onSubmitForm}>
           <input
             type='text'
             name='name'
